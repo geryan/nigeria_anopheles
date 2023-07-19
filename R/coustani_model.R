814ed6006608092f052b346175c07920ccf43917
geryan <gryan@Gerards-MacBook-Pro.local>
2023-07-19 19:04:06 +0100

    further attempts for coustani

--- R/coustani_model.R
+++ R/coustani_model.R
@@ -11,19 +11,19 @@ covs <- rast("data/grids/covariates.grd")
 nigeria_mask <- rast("data/grids/nigeria_mask.grd")
 plot(covs)
 
-df_occ <- data.frame(
-  presence_absence = rbinom(150, 1, 0.3),
-  survey_type = rep(c("IRM", "CDC", "PSC"), each = 50),
-  latitude = runif(150, 6.6, 12.4),
-  longitude = runif(150, 3.7, 9.1)
-)
+# df_occ <- data.frame(
+#   presence_absence = rbinom(150, 1, 0.3),
+#   survey_type = rep(c("IRM", "CDC", "PSC"), each = 50),
+#   latitude = runif(150, 6.6, 12.4),
+#   longitude = runif(150, 3.7, 9.1)
+# )
 
 df_all <- read_csv("data/tabular/cleaned_data.csv")
 
 df_occ <- df_all %>%
   filter(sp == target_species) %>%
   mutate(
-    presence_absence = if_else(PA == "presence", 1, 0) # this is necessary to fit model
+    presence_absence = if_else(PA == "Presence", 1, 0) # this is necessary to fit model
   )
 
 coords <- df_occ[, c("Long", "Lat")]
@@ -41,7 +41,7 @@ levels(pred_method) <- cls
 
 pred_covs <- c(covs, pred_method)
 
-df
+head(df)
 
 ## GLM
 
@@ -65,18 +65,47 @@ writeRaster(
 )
 
 ## GAM
-m_gam <- gam(presence_absence ~ survey_type + s(aspect1) + s(bio1) + s(bio10) + s(bio11) + s(bio12) + s(bio13) + s(bio14) + s(bio15) + s(bio16) + s(bio17) + s(bio18) + s(bio19) + s(bio2) + s(bio3) + s(bio4) + s(bio5) + s(bio6) + s(bio7) + s(bio8) + s(bio9) + s(elevation) + s(hillshade1) + s(slope1),
-             select = TRUE,
-             family = binomial,
-             data = df)
+# m_gam <- gam(presence_absence ~ survey_type + s(aspect1) + s(bio1) + s(bio10) + s(bio11) + s(bio12) + s(bio13) + s(bio14) + s(bio15) + s(bio16) + s(bio17) + s(bio18) + s(bio19) + s(bio2) + s(bio3) + s(bio4) + s(bio5) + s(bio6) + s(bio7) + s(bio8) + s(bio9) + s(elevation) + s(hillshade1) + s(slope1),
+#              select = TRUE,
+#              family = binomial,
+#              data = df)
+
+m_gam <- gam(
+  presence_absence ~ #survey_type +
+    s(aspect1) +
+    s(bio1) +
+    s(bio10) +
+    s(bio11) +
+    s(bio12) +
+    s(bio13) +
+    s(bio14) +
+    s(bio15) +
+    s(bio16) +
+    s(bio17) +
+    s(bio18) +
+    s(bio19) +
+    s(bio2) +
+    s(bio3) +
+    s(bio4) +
+    s(bio5) +
+    s(bio6) +
+    s(bio7) +
+    s(bio8) +
+    s(bio9) +
+    s(elevation) +
+    s(hillshade1) +
+    s(slope1),
+  select = TRUE,
+  family = binomial,
+  data = df
+)
+
 
 
 draw(m_gam) &
   theme_minimal()
 
 pred_gam <- terra::predict(pred_covs, m_gam, type = "response")
-
-
 
 plot(pred_gam)
 points(coords, col = "grey80", pch = 16)
@@ -90,4 +119,31 @@ writeRaster(
   ),
   overwrite = TRUE
 )
+
+# boosted regression tree
+library(gbm)
+library(dismo)
+m_brt_step <- gbm.step(
+  data = df,
+  gbm.x = 4:26,
+  gbm.y = 1,
+  family = "bernoulli",
+  tree.complexity = 5,
+  learning.rate = 0.01,
+  bag.fraction = 0.5
+)
+
+gbm.plot(m_brt_step, n.plots = 12)
+
+
+pred_brt <- terra::predict(
+  pred_covs,
+  m_brt_step,
+  type = "response"
+)
+
+
+plot(pred_brt)
+points(coords, col = "grey80", pch = 16)
+points(coords[df_occ$presence_absence == 1,], col = "black", pch = 16)
 
